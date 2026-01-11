313ef3f86f1a33377d5d60aa04080478c2b93344
IshtiakAnan <ishtiakahmadanan@gmail.com>
2026-01-11 13:03:22 +0600

    Clear concept about Views and urlpattern and how they work

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, welcome to the Food app!")
+
+def item(request):
+    return HttpResponse("This is an item view in the Food app.")
