0e4f0cc794e8988c43da5f1203a4cd7d8270eb99
IshtiakAnan <ishtiakahmadanan@gmail.com>
2026-03-14 15:33:49 +0600

    Login functionality added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,3 +120,6 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = 'food:index'
+LOGOUT_REDIRECT_URL = 'food:index'
--- mysite/users/admin.py
+++ mysite/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+# Unregister the default UserAdmin
+admin.site.unregister(User)
+
+# Define a new UserAdmin
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # This controls the columns in the main list view
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
